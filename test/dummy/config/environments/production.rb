329a5d0b5204b15a6269ede4f72504ca6ab971d2
Jason Lee <huacnlee@gmail.com>
2016-03-25 13:41:03 +0800

    Stdout log on production env, Heroku need that.

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
